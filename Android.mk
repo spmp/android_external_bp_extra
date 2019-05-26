c5fa5789ad142e1ed4d202d68d83aa5c125bc56f
Jasper Aorangi <jasper.mackenzie@gmail.com>
2019-05-27 11:41:05 +1200

    * Add gestures library * Add Android.mk to include gestures library

---
+++ Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libjni_latinimegoogle-prebuilt
+LOCAL_SRC_FILES := libjni_latinimegoogle.so
+include $(PREBUILT_SHARED_LIBRARY)
+

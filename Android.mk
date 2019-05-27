88510d4845fd947cb32be44cb0a4f7769c780de6
Jasper Aorangi <jasper.mackenzie@gmail.com>
2019-05-27 20:50:38 +1200

    * Correct gestures lib include

--- Android.mk
+++ Android.mk
@@ -1,8 +1,7 @@
 LOCAL_PATH := $(call my-dir)
-
+  
 include $(CLEAR_VARS)
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE := libjni_latinimegoogle-prebuilt
+LOCAL_MODULE := libjni_latinimegoogle
 LOCAL_SRC_FILES := libjni_latinimegoogle.so
 include $(BUILD_PREBUILT)
-

eab53342dddb048c8017048c4312b736e42201cf
Jasper Aorangi <jasper.mackenzie@gmail.com>
2019-05-27 13:45:21 +1200

    * Change include to LOCAL_MODULE_CLASS := SHARED_LIBRARIES

--- Android.mk
+++ Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE := libjni_latinimegoogle-prebuilt
 LOCAL_SRC_FILES := libjni_latinimegoogle.so
 include $(BUILD_PREBUILT)

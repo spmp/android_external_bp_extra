33b687c0ee382eb76116815b3e337dd19022e6cb
Jasper Aorangi <jasper.mackenzie@gmail.com>
2019-06-30 19:32:08 +1200

    Correct swype library suffix

--- Android.mk
+++ Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(call my-dir)
   
 include $(CLEAR_VARS)
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE := libjni_latinimegoogle
+LOCAL_MODULE := libjni_latinimegoogle.so
 LOCAL_SRC_FILES := libjni_latinimegoogle.so
 include $(BUILD_PREBUILT)
